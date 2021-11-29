243459732814111d5c87fdf0825f95472541b894
mauldyputra <49427045+mauldyputra@users.noreply.github.com>
2021-11-29 14:23:07 +0700

    Add files via upload

---
+++ SQLite-SwiftUI/SQLite_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLite_SwiftUIApp.swift
+//  SQLite-SwiftUI
+//
+//  Created by Mauldy Putra on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SQLite_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
